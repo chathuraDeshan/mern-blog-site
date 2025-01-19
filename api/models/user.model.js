ae51562addd141a0e780e9cba6d22e6d1a0211ed
chathuraDeshan <chathuradeshan597@gmail.com>
2025-01-19 23:10:13 +0530

    create use model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+
+    },
+
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        require: true
+    },
+
+}, {timestamps:true});
+
+const User = mongoose.model('User', userShema);
+
+export default User;
